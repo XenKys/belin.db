5a4d143b68cc2c765b1258d647ef3359890d0501
XenKys <sergiotuzzo@gmail.com>
2023-09-29 19:57:26 +0200

    Fixed push method

--- src/methods/push.ts
+++ src/methods/push.ts
@@ -7,7 +7,7 @@ export function push(self: Database, key: string, item: any): Array<any> {
   if (!Array.isArray(self.get(key)))
     throw new Error(`[belin.db] The value of '${key}' isn't an array`);
 
-  let array = self.has(key) ? [] : [];
+  let array = self.has(key) ? self.get(key) : [];
 
   array.push(item);
 
