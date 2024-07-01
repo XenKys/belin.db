81a0d6f2e69ebe0891724b4c979fda42726fbe8b
XenKys <sergiotuzzo@gmail.com>
2024-07-01 19:34:49 +0200

    fix: check if the key exists and check if the value is a number

--- src/methods/add.ts
+++ src/methods/add.ts
@@ -2,20 +2,15 @@ import type { Database } from "../Database";
 
 export function add(self: Database, key: string, number: number): number {
   if (!key) throw new Error("[belin.db] Enter a valid key");
+  if (!self.has(key))
+    throw new Error(`[belin.db] The key '${key}' doesn't exist`);
   if (number === undefined) throw new Error("[belin.db] Enter the value");
   if (isNaN(number))
     throw new TypeError("[belin.db] The value must be a number");
+  if (isNaN(self.get(key)))
+    throw new Error(`[belin.db] The value of '${key}' isn't a number`);
 
-  self.set(
-    key,
-    Number(
-      self.get(key)
-        ? isNaN(self.get(key))
-          ? Number(number)
-          : self.get(key) + Number(number)
-        : Number(number)
-    )
-  );
+  self.set(key, self.get(key) + number);
 
   return self.get(key);
 }
--- src/methods/remove.ts
+++ src/methods/remove.ts
@@ -2,19 +2,21 @@ import type { Database } from "../Database";
 
 export function remove(self: Database, key: string, number: number): number {
   if (!key) throw new Error("[belin.db] Enter a valid key");
+  if (!self.has(key))
+    throw new Error(`[belin.db] The key '${key}' doesn't exist`);
   if (number === undefined) throw new Error("[belin.db] Enter the value");
   if (isNaN(number))
     throw new TypeError("[belin.db] The value must be a number");
+  if (isNaN(self.get(key)))
+    throw new Error(`[belin.db] The value of '${key}' isn't a number`);
 
   self.set(
     key,
     self.belowZero
-      ? self.get(key) - Number(number)
-      : self.get(key)
-      ? self.get(key) - Number(number) <= 1
-        ? 1
-        : (isNaN(self.get(key)) ? 1 : self.get(key) - Number(number)) || 1
-      : 1
+      ? self.get(key) - number
+      : self.get(key) - number <= 1
+      ? 1
+      : self.get(key) - number
   );
 
   return self.get(key);
